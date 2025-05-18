c08ac89c4bf35da3901f36f9168fea229162eb2a
Verooo <yosuagerrad@gmail.com>
2025-05-19 00:22:11 +0700

    feat: Add recent sales summary and transaction cancellation count to dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Penjualan;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('be.admin.index');
+        $penjualan_terbaru = Penjualan::with(['metodeBayar', 'pelanggan', 'jenisPengiriman'])
+            ->orderBy('created_at', 'desc')
+            ->limit(3)
+            ->get();
+        return view('be.admin.index', compact('penjualan_terbaru'));
     }
 }
